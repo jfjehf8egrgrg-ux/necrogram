81031b464e4634f90ae0fe689cb576da88aea0f5
caro <jfjehf8egrgrg@gmail.com>
2026-02-13 01:07:27 +0300

    Set up Express server with a basic route

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Мессенджер работает 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
 
